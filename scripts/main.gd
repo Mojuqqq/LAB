ebc5fd967d0ee79ae1e5b908536faee8c82bb393
Mojuqqq <makarenkokl97@gmail.com>
2026-08-26 16:47:19 +0300

    player

--- scripts/main.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-func _ready():
-	var level = GameManager.current_level
-	print("Загружен уровень: ", level)
-	# Тут можно менять сложность, расположение объектов в зависимости от уровня
