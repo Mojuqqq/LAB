736ecac77ea4009fe5645882628016eeca7f8aed
Mojuqqq <makarenkokl97@gmail.com>
2026-08-26 23:26:53 +0300

    LAB 0.1

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,11 +2,18 @@ extends CharacterBody2D
 
 
 @export var speed: float = 250.0
+@export var idle_delay: float = 0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+var idle_timer: float = 0.0
 
-func _physics_process(_delta: float) -> void:
+
+func _ready() -> void:
+	animated_sprite.play("idle")
+
+
+func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector(
 		"move_left",
 		"move_right",
@@ -17,9 +24,22 @@ func _physics_process(_delta: float) -> void:
 	velocity = direction * speed
 
 	if direction != Vector2.ZERO:
+		idle_timer = 0.0
+
+		# Персонаж изначально нарисован лицом вверх.
 		rotation = direction.angle() + PI / 2
-		animated_sprite.play("walk")
+
+		# Не перезапускаем walk, если она уже идёт.
+		if animated_sprite.animation != "walk":
+			animated_sprite.play("walk")
+
 	else:
-		animated_sprite.stop()
+		idle_timer += delta
+
+		# Не переключаемся в idle мгновенно.
+		# Это позволяет короткому нажатию показать сам шаг.
+		if idle_timer >= idle_delay:
+			if animated_sprite.animation != "idle":
+				animated_sprite.play("idle")
 
 	move_and_slide()
